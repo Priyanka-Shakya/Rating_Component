9b4faa1e2914c7686ddac16273e4e0e45e585d4f
Priyanka-Shakya <priyankashakya1404@gmail.com>
2023-06-18 15:55:29 +0530

    Rating Componet

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let count = 0;
+const NumberBtn = document.querySelectorAll('.btn');
+const subBtn = document.querySelector('#subBtn');
+const show = document.querySelector('b');
+const container = document.querySelector('.container');
+const thankuBox = document.querySelector('.thankuBox');
+
+
+
+Array.from(NumberBtn).forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.className==='btn clicked'){
+            count=count+0;
+        }else{
+            button.classList.add('clicked');
+            button.style.backgroundColor = 'hsl(25, 97%, 53%)';
+            count = count+1;
+        } 
+    });
+});
+
+function rating() {
+    if(count==0){
+        alert('please rate Us!!!');
+        return;
+    }
+    container.style.display = 'none';
+    thankuBox.style.display = 'block';
+    show.innerHTML = count;
+}
+
+subBtn.addEventListener('click', rating);
